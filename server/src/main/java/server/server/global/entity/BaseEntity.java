169077824d7f98f0a9c410b3359d5d53f0fdea21
beargame <hoyoung7827@gmail.com>
2025-07-09 14:32:25 +0900

    LocalDateTime으로 변경

--- server/src/main/java/server/server/global/entity/BaseEntity.java
+++ server/src/main/java/server/server/global/entity/BaseEntity.java
@@ -6,16 +6,21 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
     @CreatedDate
     @Column(updatable = false)
-    private LocalDate createdDate;
+    private LocalDateTime createdDate;
 
     @LastModifiedDate
-    private LocalDate lastModifiedDate;
+    private LocalDateTime lastModifiedDate;
 }
