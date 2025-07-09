296d5db1bfc2d75f65bf2b6e690bc9b5b1cd609a
beargame <hoyoung7827@gmail.com>
2025-07-09 14:35:32 +0900

    웹소켓에 jwt 추가

--- server/src/main/java/server/server/global/config/WebsocketConfig.java
+++ server/src/main/java/server/server/global/config/WebsocketConfig.java
@@ -1,21 +1,30 @@
 package server.server.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import server.server.domain.gpt.service.StreamCompletionHandler;
+import server.server.global.security.filter.JwtHandshakeInterceptor;
+import server.server.global.security.jwt.JwtProvider;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSocket
-@RequiredArgsConstructor
 public class WebsocketConfig implements WebSocketConfigurer {
 
     private final StreamCompletionHandler streamCompletionHandler;
+    private final JwtProvider jwtProvider;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(streamCompletionHandler, "/chat/stream").setAllowedOrigins("*");
+        registry.addHandler(streamCompletionHandler, "/chat/stream")
+                .addInterceptors(new JwtHandshakeInterceptor(jwtProvider))
+                .setAllowedOrigins("*");
     }
 }
+
+
